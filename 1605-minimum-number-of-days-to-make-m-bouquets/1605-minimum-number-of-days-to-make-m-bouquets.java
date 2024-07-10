6e309c10a57041d0c0c6a5526d2291f4f74b1a8a
Kartik Luthra <49179437+kartiklut@users.noreply.github.com>
2024-07-10 19:28:25 +0530

    Time: 20 ms (57.44%), Space: 57.7 MB (69.42%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,41 +1,59 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        if(bloomDay.length<(long)m*k) return -1;
-        int minDays=-1;
-        int left=Integer.MAX_VALUE;
-        int right=Integer.MIN_VALUE;
-        for(int i=0;i<bloomDay.length;i++){
-           left=Math.min(left,bloomDay[i]);
-           right=Math.max(right,bloomDay[i]);
+                    //BRUTE FORCE
+    //   int maxDayPossible=Integer.MIN_VALUE;
+    //   int minDay=Integer.MAX_VALUE;
+    //   if((long)m*k>bloomDay.length)
+    //     return -1;
+    //   for(int i=0;i<bloomDay.length;i++){
+    //     maxDayPossible=Math.max(maxDayPossible,bloomDay[i]);
+    //   }
+
+    //   for(int i=1;i<=maxDayPossible;i++){
+    //     int boqFormed=bouquePossibleOnCurrDay(bloomDay,i,k);
+    //     if(boqFormed==m){
+    //         minDay=Math.min(minDay,i);
+    //     }
+    //   }
+    //   return minDay;
+                        //OPTIMAL APPROACH(BINARY SEARCH ON ANSWERS)
+      int left=Integer.MAX_VALUE; //min run loop(min->max)
+      int right=Integer.MIN_VALUE; //max
+      int minDay=Integer.MAX_VALUE;
+      if((long)m*k>bloomDay.length)
+        return -1;
+      for(int i=0;i<bloomDay.length;i++){
+        right=Math.max(right,bloomDay[i]);
+        left=Math.min(left,bloomDay[i]);
+      }
+
+      while(left<=right){
+        int mid=left+(right-left)/2;
+        int boqFormed=bouquePossibleOnCurrDay(bloomDay,mid,k);
+        if(boqFormed>=m){
+            right=mid-1;
+        }else{
+            left=mid+1;
         }
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            if(canIFormBouquetWithCurrDay(bloomDay,mid,m,k)){
-                right=mid-1;
-            }else{
-                left=mid+1;
-            }
-        }
-        return left;
+      }
+      return left;
     }
 
-    static boolean canIFormBouquetWithCurrDay(int bloomDay[],int currDay,int bouquet,int flowers){
+    static int bouquePossibleOnCurrDay(int bloomDay[],int currDay,int k){
+        int consecutive_flowers=0;
         int boqFormed=0;
-        int totalFlowers=0;
         for(int i=0;i<bloomDay.length;i++){
             if(bloomDay[i]<=currDay){
-                totalFlowers++;
+                consecutive_flowers++;
             }
-            else if(bloomDay[i]>currDay){
-                totalFlowers=0;
+            else{
+                consecutive_flowers=0;
             }
-            if(totalFlowers==flowers){
+            if(consecutive_flowers==k){
                 boqFormed++;
-                totalFlowers=0;
+                consecutive_flowers=0;
             }
-            if(boqFormed==bouquet)
-                return true;
         }
-        return false;
+        return boqFormed;
     }
 }
