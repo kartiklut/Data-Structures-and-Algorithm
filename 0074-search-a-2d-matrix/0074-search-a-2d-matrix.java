7973759041ff723a800d952d6cb35e7f25088fc3
Kartik Luthra <49179437+kartiklut@users.noreply.github.com>
2024-07-14 00:13:59 +0530

    Time: 0 ms (100%), Space: 42.2 MB (19.04%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -28,53 +28,23 @@ class Solution {
     //     return false;
     // }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
      public boolean searchMatrix(int[][] matrix, int target) {
-        int n=matrix.length;
-        int m=matrix[0].length;
-        for(int i=0;i<n;i++){
-            int left=0;
-            int right=m-1;
-            if(matrix[i][left]<=target && matrix[i][right]>=target){
-                while(left<=right){
-                    int mid=left+(right-left)/2;
-                    if(matrix[i][mid]==target)
-                        return true;
-                    else if(matrix[i][mid]<target){
-                        left=mid+1;
-                    }else{
-                        right=mid-1;
-                    }
-                }
-            }
-        }
-        return false;
-
+         // [1, 3, 5, 7],    target=13
+         // [10,11,16,20],
+         // [23,30,34,60]
+         //Make this hypothetically 1-d sorted array then apply BS
+         int n=matrix.length;
+         int m=matrix[0].length;
+         int left=0;
+         int right=n*m-1;
+         while(left<=right){
+            int mid=left+(right-left)/2;
+            int row=mid/m;
+            int col=mid%m;
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]>target) right=mid-1;
+            else left=mid+1;
+         }
+         return false;
      }
 }
