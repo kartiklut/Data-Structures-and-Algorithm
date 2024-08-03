1b372791856ce11047b369e6b746a1e04be21892
Kartik Luthra <49179437+kartiklut@users.noreply.github.com>
2024-08-04 02:58:33 +0530

    Time: 3 ms (55.63%), Space: 47.1 MB (94.6%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,19 +1,44 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        HashMap<Integer,Integer> hmap=new HashMap<>();
-        hmap.put(0,1);
-        int index=0;
+
+        //[0,0,0,0] k=0
+        //Optimal
+        int left=0;
+        int right=0;
         int count=0;
-        int prefSum=0;
-        while(index<nums.length){
-            prefSum+=nums[index];
-            int complement=prefSum-goal;
-            if(hmap.containsKey(complement)){
-                count+=hmap.get(complement);
+        int currSum=0;
+        int zerosCount=0;
+        while(right<nums.length){
+            currSum+=nums[right];
+            while(left<right && (nums[left]==0 || currSum>goal)){
+                if(nums[left]==0) zerosCount++;
+                else zerosCount=0;
+                currSum-=nums[left];
+                left++;
             }
-            hmap.put(prefSum,hmap.getOrDefault(prefSum,0)+1);
-            index++;
+            if(currSum==goal){
+                count=count+1+zerosCount;
+            }
+            right++;
         }
         return count;
+
+
+        //Better O(n) and O(goal)
+        // HashMap<Integer,Integer> hmap=new HashMap<>();
+        // hmap.put(0,1);
+        // int index=0;
+        // int count=0;
+        // int prefSum=0;
+        // while(index<nums.length){
+        //     prefSum+=nums[index];
+        //     int complement=prefSum-goal;
+        //     if(hmap.containsKey(complement)){
+        //         count+=hmap.get(complement);
+        //     }
+        //     hmap.put(prefSum,hmap.getOrDefault(prefSum,0)+1);
+        //     index++;
+        // }
+        // return count;
     }
 }
