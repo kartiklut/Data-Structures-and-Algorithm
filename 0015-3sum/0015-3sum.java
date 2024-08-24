441a01c45fe5a7e4d12cfafe307ac2ee72d338ad
Kartik Luthra <49179437+kartiklut@users.noreply.github.com>
2024-08-24 21:40:11 +0530

    Time: 47 ms (32.3%), Space: 51.4 MB (57.52%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,59 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> resAl=new ArrayList<>();
+        List<List<Integer>> resultAl=new ArrayList<>();
         Arrays.sort(nums);
-        int start=0;
-        while(start<nums.length){
-            int left=start+1;
-            int right=nums.length-1;
-            while(left<right){
-                int threeSome=nums[start]+nums[left]+nums[right];
-                if(threeSome==0){
-                    ArrayList<Integer> al=new ArrayList<Integer>(Arrays.asList(nums[start],nums[left],nums[right]));
-                    resAl.add(al);
-                }
-                if(threeSome<=0){
-                    //move left for bigger value;
-                    left++;
-                    while(threeSome<=0 && left<right && nums[left]==nums[left-1]){
-                        left++;
+        //[-4,-1,-1,0,1,2]
+    //i will be contsant j will be in mid and k will be at last for every i complete j and k to check
+    //if a[i]+a[j]+a[k]==0
+        for(int i=0;i<nums.length;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
+                if(nums[i]+nums[j]+nums[k]==0){
+                    resultAl.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    while(j<k && nums[j]==nums[j+1]){
+                        j++;
                     }
-                }else{
-                    //move right for smaller value;
-                    right--;
-                    while(threeSome>0 && left<right && nums[right]==nums[right+1]){
-                        right--;
+                    j++;
+                    while(j<k && nums[k]==nums[k-1]){
+                        k--;
                     }
+                    k--;
                 }
-            }
-            start++;
-            while(start<nums.length && nums[start]==nums[start-1]){
-                start++;
+                else if(nums[i]+nums[j]+nums[k]<0){
+                    while(j<k && nums[j]==nums[j+1]){
+                        j++;
+                    }
+                    j++;
+                }else if(nums[i]+nums[j]+nums[k]>0){
+                    while(j<k && nums[k]==nums[k-1]){
+                        k--;
+                    }
+                    k--;
+                }
             }
         }
-        return resAl;
-
-
-
-
-
-
-
-            //TLE because of extra space
-    // List<List<Integer>> resAl=new ArrayList<>();
-    //  for(int i=0;i<nums.length;i++){
-    //     HashMap<Integer,Integer> hmap=new HashMap<>();
-    //     for(int j=i+1;j<nums.length;j++){
-    //         int target=0-(nums[i]+nums[j]);
-    //         if(hmap.containsKey(target)){
-    //             ArrayList<Integer> al=new ArrayList<Integer>(Arrays.asList(nums[i],nums[j],target));
-    //             Collections.sort(al);
-    //             if(!resAl.contains(al))
-    //                 resAl.add(al);
-    //         }
-    //         hmap.put(nums[j],j);
-    //     }
-    //  }
-    //  return resAl;
+        return resultAl;
     }
 }
