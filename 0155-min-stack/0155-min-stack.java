f37643e608b1058f03ac8f0d159aa903a83faa1c
Kartik Luthra <49179437+kartiklut@users.noreply.github.com>
2024-09-09 04:22:22 +0530

    Time: 4 ms (96.02%), Space: 45.1 MB (21.27%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,57 +1,114 @@
-class Pair{
-    int key;
-    int value;
-    Pair(int key,int value){
-        this.key=key;
-        this.value=value;
-    }
-
-    int getKey(){
-        return this.key;
-    }
-
-    int getValue(){
-        return this.value;
-    }
-}
-
 class MinStack {
-
- 
-    Stack<Pair> stack=new Stack<>();
+    Stack<Long> stack=new Stack<>();
+    Long min=0L;
 
     public MinStack() {
         
     }
     
-    public void push(int val) { 
-       if(stack.isEmpty()){
-            Pair pair=new Pair(val,val);
-            stack.push(pair);
-       }else{
-            Pair prevPair=stack.peek(); 
-            Pair newPair;
-            if(prevPair.getValue()<=val){
-                newPair=new Pair(val,prevPair.getValue());
+    public void push(int val) {
+        Long vall=Long.valueOf(val);
+        if(stack.isEmpty()){
+            min=vall;
+            stack.push(vall);
+        }else{
+            if(vall<min){
+                Long newMin=2*vall-min;
+                stack.push(newMin);
+                min=vall;
             }else{
-                newPair=new Pair(val,val);
+                stack.push(vall);
             }
-            stack.push(newPair);
-       }
+        }
     }
     
     public void pop() {
-        stack.pop();
+        if(stack.peek()<=min){
+           min=2*min-stack.peek();
+           stack.pop();
+        }else{
+            stack.pop();
+        }
     }
     
     public int top() {
-        return stack.peek().getKey();
+        if(stack.peek()<=min){
+            return min.intValue();
+        }else{
+            return stack.peek().intValue();
+        }
     }
     
     public int getMin() {
-         return stack.peek().getValue();
+        return min.intValue();
     }
 }
+
+
+//Approach 1
+// class Pair{
+//     int key;
+//     int value;
+//     Pair(int key,int value){
+//         this.key=key;
+//         this.value=value;
+//     }
+
+//     int getKey(){
+//         return this.key;
+//     }
+
+//     int getValue(){
+//         return this.value;
+//     }
+// }
+
+// class MinStack {
+
+ 
+//     Stack<Pair> stack=new Stack<>();
+
+//     public MinStack() {
+        
+//     }
+    
+//     public void push(int val) { 
+//        if(stack.isEmpty()){
+//             Pair pair=new Pair(val,val);
+//             stack.push(pair);
+//        }else{
+//             Pair prevPair=stack.peek(); 
+//             Pair newPair;
+//             if(prevPair.getValue()<=val){
+//                 newPair=new Pair(val,prevPair.getValue());
+//             }else{
+//                 newPair=new Pair(val,val);
+//             }
+//             stack.push(newPair);
+//        }
+//     }
+    
+//     public void pop() {
+//         stack.pop();
+//     }
+    
+//     public int top() {
+//         return stack.peek().getKey();
+//     }
+    
+//     public int getMin() {
+//          return stack.peek().getValue();
+//     }
+// }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
 
 /**
  * Your MinStack object will be instantiated and called as such:
