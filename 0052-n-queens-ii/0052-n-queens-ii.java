a82181c08a95dcfbff4e8849387c1ec70897de16
Kartik Luthra <49179437+kartiklut@users.noreply.github.com>
2024-10-22 22:41:44 +0530

    Time: 3 ms (39.52%), Space: 40.1 MB (75.71%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -4,16 +4,16 @@ class Solution {
         return placeQueens(board,0); //pass board and col and not n cuz at the end we are iterating on row so if all rows are complete then we will have the solution which also means that all queens have been placed.
     }
 
-    public int placeQueens(int board[][],int row){
-        if(row>board.length-1){
+    public int placeQueens(int board[][],int col){
+        if(col>board.length-1){
             return 1;
         }
         int count=0;
         //as we are moving row wise horizontally will be checked automatically cuz we are only place one queen in one row
-        for(int col=0;col<board.length;col++){
+        for(int row=0;row<board.length;row++){
             if(canIplaceQueens(board,row,col)){ //pass board and places where i want to place my queen
                board[row][col]=1;//do
-               count+=placeQueens(board,row+1);//explore
+               count+=placeQueens(board,col+1);//explore
                board[row][col]=0;//undo
             }
         }
@@ -21,14 +21,14 @@ class Solution {
     }
 
     public boolean canIplaceQueens(int board[][],int row,int col){
-        //check upwards i.e vertically
-        for(int r=row;r>=0;r--){
-            if(board[r][col]==1){
+        //check upwards i.e horizontally
+        for(int c=col;c>=0;c--){
+            if(board[row][c]==1){
                 return false;
             }
         }
 
-        //check diagonally top left
+        //check diagonally top right
         //col dec. and row also dec
         for(int r=row,c=col;r>=0 && c>=0 ;r--,c--){
             if(board[r][c]==1){
@@ -37,7 +37,7 @@ class Solution {
         }
 
         //check diagonally top right
-         for(int r=row,c=col;r>=0 && c<board.length ;r--,c++){
+         for(int r=row,c=col;c>=0 && r<board.length ;c--,r++){
             if(board[r][c]==1){
                 return false;
             }
