7e9fd66568b1ee8092095f009800b2644bcd69de
Kartik Luthra <49179437+kartiklut@users.noreply.github.com>
2024-11-18 00:56:00 +0530

    Time: 0 ms (100%), Space: 41 MB (83.81%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,79 +1,41 @@
 class MyQueue {
-
-        //HEAVY POP OPERATIONS
-    Stack<Integer> stack1;
-    Stack<Integer> stack2;
+    Stack<Integer> origStack;
+    Stack<Integer> copyStack;
 
     public MyQueue() {
-        stack1=new Stack<>();
-        stack2=new Stack<>();
+        origStack=new Stack<>();
+        copyStack=new Stack<>();
     }
     
     public void push(int x) {
-        stack1.push(x);
+       origStack.push(x);
     }
     
     public int pop() {
-      if(stack2.isEmpty()){
-        while(!stack1.isEmpty()){
-            stack2.push(stack1.pop());
+        if(copyStack.isEmpty()){
+            while(!origStack.isEmpty()){
+                copyStack.push(origStack.pop());
+            }
         }
-      }
-      return stack2.pop();
+        return copyStack.pop();
 
     }
     
     public int peek() {
-        if(stack2.isEmpty()){
-            while(!stack1.isEmpty()){
-                stack2.push(stack1.pop());
+       if(copyStack.isEmpty()){
+            while(!origStack.isEmpty()){
+                copyStack.push(origStack.pop());
             }
         }
-         return stack2.peek();
+        return copyStack.peek();
     }
     
     public boolean empty() {
-         if(stack2.isEmpty()){
-            while(!stack1.isEmpty()){
-                stack2.push(stack1.pop());
-            }
+        if(origStack.isEmpty() && copyStack.isEmpty()){
+            return true;
         }
-        if(stack2.size()==0) return true;
-        else return false;
+        return false;
     }
-
-
-                    //HEAVY PUSH OPERATIONS
-    // Stack<Integer> stack1;
-    // Stack<Integer> stack2;
-
-    // public MyQueue() {
-    //     stack1=new Stack<>();
-    //     stack2=new Stack<>();
-    // }
-    
-    // public void push(int x) {
-    //     while(stack1.size()>0){
-    //         stack2.push(stack1.pop());
-    //     }
-    //     stack1.push(x);
-    //     while(stack2.size()>0){
-    //         stack1.push(stack2.pop());
-    //     }
-    // }
-    
-    // public int pop() {
-    //    return stack1.pop();
-    // }
-    
-    // public int peek() {
-    //     return stack1.peek();
-    // }
-    
-    // public boolean empty() {
-    //     if(stack1.size()==0) return true;
-    //     else return false;
-    // }
 }
 
 /**
