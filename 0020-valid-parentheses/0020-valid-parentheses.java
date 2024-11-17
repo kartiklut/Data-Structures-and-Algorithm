b3fbbaca6aa0742d5ee2761c7953f5867a9abb1d
Kartik Luthra <49179437+kartiklut@users.noreply.github.com>
2024-11-18 01:15:53 +0530

    Time: 3 ms (43.16%), Space: 41.7 MB (28.77%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,18 +1,27 @@
 class Solution {
     public boolean isValid(String s) {
+        //whichever is opened recently should be closed first that's it
         Stack<Character> stack=new Stack<>();
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)==')' && !stack.isEmpty() && stack.peek()=='('){
-                stack.pop();
-            }else if(s.charAt(i)==']' && !stack.isEmpty() && stack.peek()=='['){
-                stack.pop();
-            }else if(s.charAt(i)=='}' && !stack.isEmpty() && stack.peek()=='{'){
-                stack.pop();
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                stack.push(s.charAt(i));
+            }else if(stack.isEmpty() && (s.charAt(i)==')' || s.charAt(i)=='}' || s.charAt(i)==']')){
+                return false;
             }else{
-                 stack.push(s.charAt(i));
+                if(s.charAt(i)==')' && !stack.isEmpty() && stack.peek()=='('){
+                    stack.pop();
+                }else if(s.charAt(i)==']' && !stack.isEmpty() && stack.peek()=='['){
+                    stack.pop();
+                }else if(s.charAt(i)=='}' && !stack.isEmpty() && stack.peek()=='{'){
+                    stack.pop();
+                }else{
+                    return false;
+                }
             }
         }
-        if(stack.isEmpty()) return true;
+        if(stack.isEmpty()){
+            return true;
+        }
         return false;
     }
 }
