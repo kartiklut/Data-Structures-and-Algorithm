f0a864e8c6577301a03b80ac9a94d67c6186c257
Kartik Luthra <49179437+kartiklut@users.noreply.github.com>
2024-11-18 03:08:23 +0530

    Time: 17 ms (47.14%), Space: 46.4 MB (19.42%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,34 +1,30 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
+        int res[]=new int[nums.length];
         Stack<Integer> stack=new Stack<>();
-        int nge[]=new int[nums.length];
         for(int i=2*nums.length-1;i>=0;i--){
-            if(i>=nums.length){
-                if(stack.isEmpty()){
-                    stack.push(nums[i%nums.length]);
-                }else{
-                    while(!stack.isEmpty() && stack.peek()<=nums[i%nums.length]){
-                        stack.pop();
-                    }
-                    stack.push(nums[i%nums.length]);
+            int index=i%nums.length;
+            if(stack.isEmpty()){
+                stack.push(nums[index]);
+                if(i<nums.length){
+                    res[index]=-1;
                 }
             }else{
-                if(stack.isEmpty()){
-                    nge[i]=-1;
-                    stack.push(nums[i]);
-                }else{
-                    while(!stack.isEmpty() && stack.peek()<=nums[i]){
+                if(nums[index]>=stack.peek()){
+                    while(!stack.isEmpty() && nums[index]>=stack.peek()){
                         stack.pop();
                     }
-                    if(stack.isEmpty()){
-                        nge[i]=-1;
-                    }else{
-                        nge[i]=stack.peek();
+                    if(stack.isEmpty() && i<nums.length){
+                        res[index]=-1;
+                    }else if(i<nums.length){
+                        res[index]=stack.peek();
                     }
-                    stack.push(nums[i]);
+                }else if(i<nums.length){
+                        res[index]=stack.peek();
                 }
+                stack.push(nums[index]);
             }
         }
-        return nge;
+        return res;
     }
 }
