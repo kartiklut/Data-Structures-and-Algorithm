5e008d32c01b058f832ad1c80e61fe1c68c2d00f
Kartik Luthra <49179437+kartiklut@users.noreply.github.com>
2024-11-22 09:39:04 +0530

    Time: 230 ms (5.12%), Space: 48.2 MB (5.77%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,66 +1,79 @@
 class Solution {
+    // 0 1
+    // 1 0
     public int maximalRectangle(char[][] matrix) {
-        int heights[]=new int[matrix[0].length];
-        for(int i=0;i<matrix[0].length;i++){
-            heights[i]=matrix[0][i]=='1' ? 1 : 0;
+        int max=Integer.MIN_VALUE;
+        for(int row=0;row<matrix.length;row++){
+            max=Math.max(calcMaxArea(row,matrix),max);
         }
-        int maxArea=calculateMaxArea1D(heights);
-        for(int i=1;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]=='0'){
-                    heights[j]=0;
-                }else{          
-                    heights[j]+=1;
-                }   
-            }
-            maxArea=Math.max(maxArea,calculateMaxArea1D(heights));
-        }
-        return maxArea;
+        return max;
     }
 
-    int calculateMaxArea1D(int heights[]){
-        int area=0;
+    public int calcMaxArea(int row,char matrix[][]){
         Stack<Integer> stack=new Stack<>();
-        int pse[]=new int[heights.length];
-        int nse[]=new int[heights.length];
-        for(int i=0;i<heights.length;i++){
-            if(stack.isEmpty()){
-                stack.push(i);
-                pse[i]=-1;
-                continue;
+        if(row!=0){
+            for(int i=0;i<matrix[0].length;i++){
+                int prevRowEle=matrix[row-1][i]-'0';
+                if(matrix[row][i]!='0'){
+                    matrix[row][i]+=prevRowEle;
+                }
             }
-            while(!stack.isEmpty() && heights[stack.peek()]>=heights[i]){
-                stack.pop();
-            }
-            if(stack.isEmpty()){
-                pse[i]=-1;
-            }else{
-                pse[i]=stack.peek();
-            }
-            stack.push(i);
         }
+        int pse[]=calcPse(row,matrix,stack);
         stack=new Stack<>();
-        for(int i=heights.length-1;i>=0;i--){
+        int nse[]=calcNse(row,matrix,stack);
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<matrix[0].length;i++){
+            int height=matrix[row][i]-'0';
+            System.out.println(height);
+            int psElement=pse[i]+1;
+            int nsElement=nse[i]-1;
+            int currWidth=nsElement-psElement;
+            int width=currWidth+1;
+            max=Math.max(max,height*width);
+        }
+        return max;
+    }
+
+    public int[] calcPse(int row,char matrix[][],Stack<Integer> stack){
+        int pse[]=new int[matrix[0].length];
+        for(int i=0;i<matrix[0].length;i++){
             if(stack.isEmpty()){
-                nse[i]=heights.length;
+                pse[i]=-1;
                 stack.push(i);
-                continue;
-            }
-            while(!stack.isEmpty() && heights[stack.peek()]>=heights[i]){
-                stack.pop();
-            }
-            if(stack.isEmpty()){
-                nse[i]=heights.length;
             }else{
-                nse[i]=stack.peek();
+                while(!stack.isEmpty() && matrix[row][stack.peek()]>=matrix[row][i]){
+                    stack.pop();
+                }
+                if(stack.isEmpty()){
+                    pse[i]=-1;
+                }else{
+                    pse[i]=stack.peek();
+                }
+                stack.push(i);
             }
-            stack.push(i);
         }
-        //calc area
-        //nse[i]-pse[i]-1
-        for(int i=0;i<heights.length;i++){
-            area=Math.max(area,heights[i]*(nse[i]-pse[i]-1));
+        return pse;
+    }
+
+    public int[] calcNse(int row,char matrix[][],Stack<Integer> stack){
+        int nse[]=new int[matrix[0].length];
+        for(int i=matrix[0].length-1;i>=0;i--){
+            if(stack.isEmpty()){
+                nse[i]=matrix[0].length;
+                stack.push(i);
+            }else{
+                while(!stack.isEmpty() && matrix[row][stack.peek()]>=matrix[row][i]){
+                    stack.pop();
+                }
+                if(stack.isEmpty()){
+                    nse[i]=matrix[0].length;
+                }else{
+                    nse[i]=stack.peek();
+                }
+                stack.push(i);
+            }
         }
-        return area;
+        return nse;
     }
 }
